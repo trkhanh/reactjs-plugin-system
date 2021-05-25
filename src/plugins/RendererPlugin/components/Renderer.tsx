93aa8d7bdf4680c609081c7a1a5c86173080d108
Tran Khanh <tran_khanh@outlook.com>
2021-05-25 16:16:18 +0700

    implement plugin

---
+++ src/plugins/RendererPlugin/components/Renderer.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import useForceUpdate from '../../../hooks/useForceUpdate';
+import { usePluginStore } from '../../../hooks/usePluginStore';
+import ComponentUpdatedEvent from '../events/ComponentUpdatedEvent';
+
+export const Renderer: React.SFC<{
+  placement: string;
+}> = ({ placement }) => {
+  const pluginStore = usePluginStore();
+  const forceUpdate = useForceUpdate();
+
+  useEffect(() => {
+    const eventListener = (event: ComponentUpdatedEvent) => {
+      if (event.position === placement) {
+        forceUpdate();
+      }
+    };
+    pluginStore.addEventListener('Renderer.componentUpdated', eventListener);
+
+    return () => {
+      pluginStore.removeEventListener(
+        'Renderer.componentUpdated',
+        eventListener
+      );
+    };
+  }, [pluginStore, placement, forceUpdate]);
+
+  let components = pluginStore.executeFunction(
+    'Renderer.getComponentsInPosition',
+    placement
+  );
+
+  return (
+    <>
+      {components.map(
+        (compObject: { component: React.ComponentClass; key: string }) => (
+          <compObject.component key={compObject.key} />
+        )
+      )}
+    </>
+  );
+};
